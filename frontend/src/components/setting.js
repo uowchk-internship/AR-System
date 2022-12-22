1d4e10d25aa1434722b56fd162e732fa71ece7ab
unclechuen <johnny@johnnyip.com>
2022-12-23 04:06:19 +0800

    Update setting.js

--- frontend/src/components/setting.js
+++ frontend/src/components/setting.js
@@ -19,7 +19,7 @@ const SettingComponent = (props) => {
             <div style={{ backgroundColor: 'lightgrey', padding: 10, borderRadius: 10 }}>
                 <Button
                     onClick={() => {
-                        dispatch(setURL((process.env.REACT_APP_SERVER_URL !== undefined) ? process.env.REACT_APP_SERVER_URL : 'http://localhost:8080'))
+                        dispatch(setURL("http://localhost:8080"))
                         setSignedIn(false)
                         logout()
 
