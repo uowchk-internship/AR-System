745a10daf914a178e5f02cf2f61f48946667be0e
unclechuen <johnny@johnnyip.com>
2022-12-24 23:41:45 +0800

    Update examHome.js

--- frontend/src/components/reports/exam/examHome.js
+++ frontend/src/components/reports/exam/examHome.js
@@ -1,5 +1,8 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 import { SegmentedControl } from '@mantine/core';
-import { useState } from 'react';
+
+import { updateHashmap } from "../../../functions/report/degreeworks";
 
 import TimeTable from './timeTable';
 import Information from './information'
@@ -7,11 +10,30 @@ import AttendanceRegister from './attendanceRegister'
 import ExamBoard from './board'
 
 function ExamHomeComponent(props) {
+    const { url } = useSelector((state) => state.setting);
+
     let username_ = props.username
     let username = (username_ === "demo") ? "admin" : username_
 
     //tab state
     const [activeTab, setActiveTab] = useState("timetable");
+    const [loaded, setLoaded] = useState(false);
+    const [hashed, setHashed] = useState(false);
+
+    useEffect(() => {
+        const loadData = async () => {
+            setLoaded(true);
+            updateHashmap(url);
+            setTimeout(() => {
+                setHashed(true);
+            }, 5000);
+
+        };
+
+        if (!loaded) {
+            loadData();
+        }
+    })
 
     return (
         <>
@@ -28,14 +50,18 @@ function ExamHomeComponent(props) {
                     ]} />
 
                 {
-                    (activeTab === "info") ?
-                        <Information username={username} /> :
-                        (activeTab === "timetable") ?
-                            <TimeTable username={username} /> :
-                            (activeTab === "attendance") ?
-                                <AttendanceRegister username={username} /> :
-                                (activeTab === "board") ?
-                                    <ExamBoard username={username} /> : <></>
+                    ((!hashed) ?
+                        <h1>Loading... (wait 6 seconds)</h1> :
+                        (activeTab === "info") ?
+                            <Information username={username} /> :
+                            (activeTab === "timetable") ?
+                                <TimeTable username={username} /> :
+                                (activeTab === "attendance") ?
+                                    <AttendanceRegister username={username} /> :
+                                    (activeTab === "board") ?
+                                        <ExamBoard username={username} /> : <></>
+
+                    )
                 }
             </div>
 
